52f1660fe052acdcb53ab1b4754b17d370772677
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-24 21:02:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const employees = ["Ada", "Brendan", "Ali"];
+
+function printBadges(employees) {
+  for (let i=0; i < employees.length; i++){
+      console.log('Welcome ${employees[i]}! You are employee #${i}.');
+  }
+  return employees;
+}
+
+printBadges(employees);
