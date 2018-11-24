045e5670bd55494efb4424cf633a74037d345b30
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-24 22:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ printBadges(employees);
 
 function tailsNeverFails(){
   let tails = 0;
-  while (){
-    return Math.random() >= 0.5;
+  while (Math.random() >= 0.5){
+    tails++;
   }
+  return 'You got ${tails} tails in a row!'
 };
